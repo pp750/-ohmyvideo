6ea218394c8bb25d4cc809a0cb1d38e529c25bb3
pp750 <750878016@qq.com>
2026-05-11 22:51:04 +0800

    debug: 注释调试代码 (env.ts)
    - 移除调试输出

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
-// 判断是否为打包后的 Electron 环境
+﻿// 判断是否为打包后的 Electron 环境
 const isElectron = typeof process.versions?.electron !== "undefined";
 let isPackaged = false;
 if (isElectron) {
@@ -11,5 +11,5 @@ const env = process.env.NODE_ENV;
 if (!env) {
   if (isElectron) process.env.NODE_ENV = "prod";
   else process.env.NODE_ENV = "dev";
-  console.log(`[环境变量：${process.env.NODE_ENV}]`);
+  // console.log(`[环境变量：${process.env.NODE_ENV}]`);
 }
