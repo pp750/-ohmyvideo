4992b2fb1e64d9709709d92843dabb39611de2a5
pp750 <750878016@qq.com>
2026-05-11 22:51:23 +0800

    perf: 禁用控制台输出 (db.ts)
    - 提升性能

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -14,7 +14,7 @@ type TableName = keyof DB & string;
 type RowType<TName extends TableName> = DB[TName];
 
 const dbPath = getPath("db2.sqlite");
-console.log("数据库目录:", dbPath);
+// console.log("数据库目录:", dbPath);
 const dbDir = path.dirname(dbPath);
 
 
